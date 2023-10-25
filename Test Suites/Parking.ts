6dcfd364594636841a2069d8c3d473f65b28b80a
mykel0127 <148882240+mykel0127@users.noreply.github.com>
2023-10-25 09:07:11 +0800

    Add files via upload

---
+++ Test Suites/Parking.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Parking</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0422db7e-3e1d-4e32-9ea1-e516b6c13eb5</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e623660-b02b-410a-9c31-c0425dff7ba3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Parking/Parking</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
